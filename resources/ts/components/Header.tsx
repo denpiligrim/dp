dc6dca73771f356c2f0c37d6aa3dfd81064460bb
vasiljevdenis <vasiljevdenis@bk.ru>
2024-12-10 00:46:07 +0300

    header mobile

--- resources/ts/components/Header.tsx
+++ resources/ts/components/Header.tsx
@@ -1,22 +1,51 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Box, Link, IconButton, Menu, MenuItem } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { AppBar, Toolbar, Typography, Box, Link, IconButton, Menu, MenuItem, useTheme, useMediaQuery } from '@mui/material';
 import CodeIcon from '@mui/icons-material/Code';
 import LanguageIcon from '@mui/icons-material/Language';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import "../i18n";
 import RuIcon from '../svgIcons.ts/RuIcon';
 import UkIcon from '../svgIcons.ts/UkIcon';
+import axios from 'axios';
 
 const Header: React.FC = () => {
   const navigator = useNavigate();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   const { t, i18n } = useTranslation();
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLButtonElement>(null);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLButtonElement>(null);
   const open = Boolean(anchorEl);
+  const [anchorEl2, setAnchorEl2] = useState<null | HTMLButtonElement>(null);
+  const open2 = Boolean(anchorEl2);
 
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng); // Смена языка
   };
+
+  useEffect(() => {
+    const savedLocale = localStorage.getItem("lng");
+    if (savedLocale) {
+      changeLanguage(savedLocale);
+    }
+
+    try {
+      axios.get('/get-user-info')
+        .then(res => {
+          const data = res.data;
+          console.log(data);
+
+          const userLocale = data.country === "RU" ? "ru" : "en"; // Проверка региона
+          localStorage.setItem("lng", userLocale); // Сохраняем локаль в localStorage
+          changeLanguage(userLocale);
+        })
+        .catch(err => console.log(err))
+    } catch (error) {
+      console.error("Ошибка определения локали:", error);
+      changeLanguage('en');
+    }
+  }, []);
 
   return (
     <AppBar position="static" color="primary">
@@ -55,24 +84,60 @@ const Header: React.FC = () => {
               RU
             </MenuItem>
           </Menu>
-          <Link href="/blog" color="inherit" underline="hover" sx={{ mx: 2, verticalAlign: 'middle' }} onClick={(e) => {
-            e.preventDefault();
-            navigator('/blog');
-          }}>
-            {t("headerBlog")}
-          </Link>
-          <Link href="/dev" color="inherit" underline="hover" sx={{ mr: 2, verticalAlign: 'middle' }} onClick={(e) => {
-            e.preventDefault();
-            navigator('/dev');
-          }}>
-            {t("headerDev")}
-          </Link>
-          <Link href="/crypto" color="inherit" underline="hover" sx={{ verticalAlign: 'middle' }} onClick={(e) => {
-            e.preventDefault();
-            navigator('/crypto');
-          }}>
-            {t("headerCrypto")}
-          </Link>
+          {isMobile ? (
+            <>
+              <IconButton
+                onClick={(e) => setAnchorEl2(e.target as HTMLButtonElement)}
+              >
+                <MoreVertIcon />
+              </IconButton>
+              <Menu
+                anchorEl={anchorEl2}
+                open={open2}
+                onClose={(e) => setAnchorEl2(null)}
+              >
+                <MenuItem onClick={() => {
+                  navigator('/blog');
+                  setAnchorEl2(null);
+                }}>
+                  {t("headerBlog")}
+                </MenuItem>
+                <MenuItem onClick={() => {
+                  navigator('/dev');
+                  setAnchorEl2(null);
+                }}>
+                  {t("headerDev")}
+                </MenuItem>
+                <MenuItem onClick={() => {
+                  navigator('/crypto');
+                  setAnchorEl2(null);
+                }}>
+                  {t("headerCrypto")}
+                </MenuItem>
+              </Menu>
+            </>
+          ) : (
+            <>
+              <Link href="/blog" color="inherit" underline="hover" sx={{ mx: 2, verticalAlign: 'middle' }} onClick={(e) => {
+                e.preventDefault();
+                navigator('/blog');
+              }}>
+                {t("headerBlog")}
+              </Link>
+              <Link href="/dev" color="inherit" underline="hover" sx={{ mr: 2, verticalAlign: 'middle' }} onClick={(e) => {
+                e.preventDefault();
+                navigator('/dev');
+              }}>
+                {t("headerDev")}
+              </Link>
+              <Link href="/crypto" color="inherit" underline="hover" sx={{ verticalAlign: 'middle' }} onClick={(e) => {
+                e.preventDefault();
+                navigator('/crypto');
+              }}>
+                {t("headerCrypto")}
+              </Link>
+            </>
+          )}
         </Box>
       </Toolbar>
     </AppBar>
--- resources/ts/i18n.ts
+++ resources/ts/i18n.ts
@@ -1,45 +1,19 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import translations from "./locales";
-import axios from "axios";
 
-const getUserLocale = async () => {
-  const savedLocale = localStorage.getItem("lng");
-  if (savedLocale) {
-    return savedLocale; // Используем сохранённую локаль
-  }
-
-  try {
-    axios.get('/get-user-info')
-      .then(res => {
-        const data = res.data;
-
-        const userLocale = data.country === "RU" ? "ru" : "en"; // Проверка региона
-        localStorage.setItem("lng", userLocale); // Сохраняем локаль в localStorage
-        return userLocale;
-      })
-      .catch(err => console.log(err))
-  } catch (error) {
-    console.error("Ошибка определения локали:", error);
-    return "en"; // Локаль по умолчанию в случае ошибки
-  }
-};
-
-getUserLocale()
-.then(lng => {
-  i18n
+i18n
   .use(initReactI18next)
   .init({
     resources: {
       en: { translation: translations.en },
       ru: { translation: translations.ru },
     },
-    lng: lng, // Устанавливаем локаль
+    lng: 'en', // Устанавливаем локаль
     fallbackLng: "en",
     interpolation: {
       escapeValue: false,
     },
   });
-}); // Получаем локаль
 
 export default i18n;
